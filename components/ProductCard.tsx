af2bad67380e6abac5bd306298c3fe39d172ee22
user <samuel_morgan_@outlook.com>
2023-08-17 14:54:35 -0300

    product card styles

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -4,26 +4,37 @@ const ProductCard = ({ product, campaign_handle }) => {
   const { minVariantPrice, maxVariantPrice } = product.priceRangeV2;
 
   return (
-    <a
-      href={`/campaign/${campaign_handle}/products/${
-        product.id.split("gid://shopify/Product/")[1]
-      }`}
-      className="border border-solid border-black overflow-hidden w-fit mx-auto bg-white"
-    >
-      <img
-        className="w-48 h-48 object-cover max-w-full border-0 border-b border-solid border-black"
-        src={product.featuredImage?.url}
-      />
-      <div className="flex flex-col w-48 p-3">
-        <h3 className="truncate overflow-hidden">{product.title}</h3>
-        <div className="flex flex-row">
+    <div className="overflow-hidden w-fit mx-auto bg-white">
+      <a
+        href={`/campaign/${campaign_handle}/products/${
+          product.id.split("gid://shopify/Product/")[1]
+        }`}
+        className="flex h-fit "
+      >
+        <img
+          className="w-48 h-48 object-cover max-w-full"
+          src={product.featuredImage?.url}
+        />
+      </a>
+      <div className="flex flex-col w-48 py-2 text-base">
+        <a
+          href={`/campaign/${campaign_handle}/products/${
+            product.id.split("gid://shopify/Product/")[1]
+          }`}
+          className="no-underline"
+        >
+          <h3 className="text-black truncate overflow-hidden font-bold	">
+            {product.title}
+          </h3>
+        </a>
+        <div className="flex flex-row pt-1">
           <h3 className="line-through	mr-1">
             {currencyFormatter(maxVariantPrice)}
           </h3>
-          <h3>{currencyFormatter(minVariantPrice)}</h3>
+          <h3 className="font-bold	">{currencyFormatter(minVariantPrice)}</h3>
         </div>
       </div>
-    </a>
+    </div>
   );
 };
 
