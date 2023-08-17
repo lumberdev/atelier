20d5ba1f2153bd8f7ae26ab59393b9cc39f757a8
user <samuel_morgan_@outlook.com>
2023-08-17 15:46:28 -0300

    removed inline style files

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,23 +4,10 @@ import Link from "next/link";
 const NavBar = ({ campaign, campaign_handle }) => {
   return (
     <>
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
-      />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap"
-        rel="stylesheet"
-      />
       <Section>
         <div className="flex flex-col items-center justify-center">
           <Link href={`/campaign/${campaign_handle}`}>
-            <h1
-              style={{ fontFamily: "Pacifico" }}
-              className="text-4xl mx-1 my-8"
-            >
-              {campaign?.title}
-            </h1>
+            <h1 className="text-4xl mx-1 my-8">{campaign?.title}</h1>
           </Link>
           <nav className="flex flex-row my-8">
             {["Dresses", "Jeans", "Hat"].map((linkTitle) => (
