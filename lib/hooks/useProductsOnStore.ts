7210bebfc0d9dfe507eceb689a06cbd8f6fa0e66
user <samuel_morgan_@outlook.com>
2023-08-17 23:06:48 -0300

    useProducts doesnt run tin store id

--- lib/hooks/useProductsOnStore.ts
+++ lib/hooks/useProductsOnStore.ts
@@ -3,23 +3,28 @@ import { useQuery } from "react-query";
 export const useProductsOnStore = ({ store_id }) => {
   const { data = { products: [] }, isLoading } = useQuery<{
     products: any[];
-  }>(["products", store_id], () =>
-    fetch(`/api/products?store_id=${store_id}`)
-      .then((response) => response.json())
-      .then((data) => {
-        return {
-          products: data.products.map((product) => {
-            return {
-              id: product.node.id,
-              title: product.node.title,
-              priceRangeV2: product.node.priceRangeV2,
-              updatedAt: product.node.updatedAt,
-              handle: product.node.handle,
-              featuredImage: product.node.featuredImage,
-            };
-          }),
-        };
-      })
+  }>(
+    ["products", store_id],
+    () =>
+      fetch(`/api/products?store_id=${store_id}`)
+        .then((response) => response.json())
+        .then((data) => {
+          return {
+            products: data.products.map((product) => {
+              return {
+                id: product.node.id,
+                title: product.node.title,
+                priceRangeV2: product.node.priceRangeV2,
+                updatedAt: product.node.updatedAt,
+                handle: product.node.handle,
+                featuredImage: product.node.featuredImage,
+              };
+            }),
+          };
+        }),
+    {
+      enabled: !!store_id,
+    }
   );
 
   return {
