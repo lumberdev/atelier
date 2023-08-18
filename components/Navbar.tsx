064537cb4c27451e9b4521146f9eea758d52198f
user <samuel_morgan_@outlook.com>
2023-08-17 23:16:14 -0300

    no fragment

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,26 +3,24 @@ import Link from "next/link";
 
 const NavBar = ({ campaign, campaign_handle }) => {
   return (
-    <>
-      <Section>
-        <div className="flex flex-col items-center justify-center">
-          <Link href={`/campaign/${campaign_handle}`} className="no-underline ">
-            <h1 className="text-4xl text-black mx-1 my-8">{campaign?.title}</h1>
-          </Link>
-          <nav className="flex flex-row my-8">
-            {["Dresses", "Jeans", "Hat"].map((linkTitle) => (
-              <Link
-                href="/"
-                className="mx-8 no-underline text-xl text-black"
-                key={linkTitle}
-              >
-                {linkTitle}
-              </Link>
-            ))}
-          </nav>
-        </div>
-      </Section>
-    </>
+    <Section>
+      <div className="flex flex-col items-center justify-center">
+        <Link href={`/campaign/${campaign_handle}`} className="no-underline ">
+          <h1 className="text-4xl text-black mx-1 my-8">{campaign?.title}</h1>
+        </Link>
+        <nav className="flex flex-row my-8">
+          {["Dresses", "Jeans", "Hat"].map((linkTitle) => (
+            <Link
+              href="/"
+              className="mx-8 no-underline text-xl text-black"
+              key={linkTitle}
+            >
+              {linkTitle}
+            </Link>
+          ))}
+        </nav>
+      </div>
+    </Section>
   );
 };
 
