7c2ebdc4e9e7ab8b7fc2186648e3c7dc4e4a00e9
user <samuel_morgan_@outlook.com>
2023-08-17 23:11:50 -0300

    useProductOnStore enabled flag

--- lib/hooks/useProductOnStore.ts
+++ lib/hooks/useProductOnStore.ts
@@ -3,22 +3,27 @@ import { useQuery } from "react-query";
 export const useProductOnStore = ({ store_id, product_id }) => {
   const { data = { product: {} }, isLoading } = useQuery<{
     product: any;
-  }>(["product", store_id, product_id], () =>
-    fetch(`/api/product?store_id=${store_id}&product_id=${product_id}`)
-      .then((response) => response.json())
-      .then((data) => {
-        return {
-          product: {
-            id: data.product.id,
-            title: data.product.title,
-            description: data.product.description,
-            priceRangeV2: data.product.priceRangeV2,
-            options: data.product.options,
-            handle: data.product.handle,
-            images: data.product.images.edges.map((edge) => edge.node),
-          },
-        };
-      })
+  }>(
+    ["product", store_id, product_id],
+    () =>
+      fetch(`/api/product?store_id=${store_id}&product_id=${product_id}`)
+        .then((response) => response.json())
+        .then((data) => {
+          return {
+            product: {
+              id: data.product.id,
+              title: data.product.title,
+              description: data.product.description,
+              priceRangeV2: data.product.priceRangeV2,
+              options: data.product.options,
+              handle: data.product.handle,
+              images: data.product.images.edges.map((edge) => edge.node),
+            },
+          };
+        }),
+    {
+      enabled: !!store_id && !!product_id,
+    }
   );
   return {
     isLoading,
