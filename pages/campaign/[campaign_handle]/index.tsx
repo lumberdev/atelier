55f92b88a69e03492f6933e51a4fdb625259a227
user <samuel_morgan_@outlook.com>
2023-08-17 23:03:56 -0300

    root page loads everything first

--- pages/campaign/[campaign_handle]/index.tsx
+++ pages/campaign/[campaign_handle]/index.tsx
@@ -2,28 +2,34 @@ import NavBar from "../../../components/Navbar";
 import ProductGrid from "../../../components/ProductGrid";
 import { useRouter } from "next/router";
 import { useCampaignOnStore } from "@/lib/hooks/useCampaignOnStore";
+import { useProductsOnStore } from "@/lib/hooks/useProductsOnStore";
 import Spinner from "@/components/Spinner";
 
 const HomeCampaignPage = () => {
   const router = useRouter();
   const { campaign_handle } = router.query;
   const campaign = useCampaignOnStore({ campaign_handle }).campaign;
+  const productIDs = campaign?.resourceIds;
+  const { products: allProducts, isLoading: productsLoading } =
+    useProductsOnStore({
+      store_id: campaign.storeId,
+    });
+  const products = allProducts.filter((product) =>
+    productIDs.includes(product.id)
+  );
+
+  if (productsLoading) {
+    return (
+      <div className="h-screen flex flex-col items-center justify-center">
+        <Spinner />
+      </div>
+    );
+  }
 
   return (
     <div className="flex flex-col items-center justify-center">
-      {campaign ? (
-        <>
-          <NavBar {...{ campaign, campaign_handle }} />
-          <ProductGrid {...{ campaign, campaign_handle }} />
-        </>
-      ) : (
-        <div className="h-screen flex flex-col items-center justify-center">
-          Loading campaign
-          <br />
-          <br />
-          <Spinner />
-        </div>
-      )}
+      <NavBar {...{ campaign, campaign_handle }} />
+      <ProductGrid {...{ products, campaign_handle }} />
     </div>
   );
 };
