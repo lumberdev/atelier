c174b56f407cdf66e7ff0981c9353468c1f43d6c
Luis Fernando Saavedra Meza <fernando@fsvdr.me>
2023-09-25 18:47:11 -0600

    db: Add global theme table

---
+++ prisma/migrations/20230926004557_add_store_theme/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "storeThemes" (
+    "id" TEXT NOT NULL,
+    "storeId" TEXT NOT NULL,
+    "logo" TEXT,
+    "primaryColor" TEXT,
+    "secondaryColor" TEXT,
+    "backgroundColor" TEXT,
+    "borderRadius" INTEGER DEFAULT 0,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "storeThemes_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "storeThemes_storeId_key" ON "storeThemes"("storeId");
+
+-- AddForeignKey
+ALTER TABLE "storeThemes" ADD CONSTRAINT "storeThemes_storeId_fkey" FOREIGN KEY ("storeId") REFERENCES "stores"("shop") ON DELETE RESTRICT ON UPDATE CASCADE;
