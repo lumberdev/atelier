53006e983194d4d0bbce9584b2c76ff0695b2a38
user <samuel_morgan_@outlook.com>
2023-09-27 16:39:24 -0300

    added collection page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,9 +9,18 @@ const NavBar = ({ campaign, campaignHandle, collections }) => {
           <h1 className="text-4xl text-black mx-1 my-8">{campaign?.title}</h1>
         </Link>
         <nav className="flex flex-row my-8">
-          {collections.map(({ title, handle }) => (
+          <Link
+            href={`/campaign/${campaignHandle}/`}
+            className="mx-8 no-underline text-xl text-black"
+            key={"all"}
+          >
+            Shop All
+          </Link>
+          {collections.map(({ title, handle, id }) => (
             <Link
-              href={`/campaign/${campaignHandle}/collections/${handle}`}
+              href={`/campaign/${campaignHandle}/collections/${
+                id.match(/\d+/)[0] || id
+              }`}
               className="mx-8 no-underline text-xl text-black"
               key={handle}
             >
---
+++ pages/campaign/[handle]/collections/[collection_id]/index.tsx
@@ -0,0 +1,39 @@
+import { useRouter } from "next/router";
+import { useCampaignOnStore } from "@/lib/hooks/useCampaignOnStore";
+import { useCollectionsOnStore } from "@/lib/hooks/useCollectionsOnStore";
+import NavBar from "@/components/Navbar";
+import ProductGrid from "@/components/ProductGrid";
+import LoadingScreen from "@/components/LoadingScreen";
+import Page from "@/components/Page";
+
+const CollectionCampaignPage = () => {
+  const router = useRouter();
+  const { handle, collection_id } = router.query;
+
+  const { isLoading: campaignLoading, campaign } = useCampaignOnStore({
+    handle,
+  });
+  const { collections, isLoading: collectionsLoading } = useCollectionsOnStore({
+    store_id: campaign.storeId,
+    collection_ids: campaign?.collectionIds,
+  });
+
+  if (campaignLoading || collectionsLoading) return <LoadingScreen />;
+
+  const selectedCollection = collections.find(
+    (collection) => collection.id.match(/\d+/)[0] === collection_id
+  );
+  const collectionProducts = selectedCollection?.products;
+
+  return (
+    <Page>
+      <NavBar {...{ campaign, campaignHandle: handle, collections }} />
+      <h1 className="text-4xl text-black mx-1 my-8 mx-16 mr-auto ">
+        {selectedCollection?.title}
+      </h1>
+      <ProductGrid {...{ products: collectionProducts, handle }} />
+    </Page>
+  );
+};
+
+export default CollectionCampaignPage;
