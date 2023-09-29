9dc222648fa156ff2fa9478cd163c53990328eb0
user <samuel_morgan_@outlook.com>
2023-09-29 12:40:20 -0300

    product card font size

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -24,15 +24,17 @@ const ProductCard = ({ product, handle }) => {
           }`}
           className="no-underline"
         >
-          <h3 className="text-black truncate overflow-hidden font-bold	">
+          <h3 className="text-black truncate overflow-hidden font-bold text-base sm:text-xl">
             {product.title}
           </h3>
         </Link>
         <div className="flex flex-row pt-1">
-          <h3 className="line-through	mr-1">
+          <h3 className="line-through	mr-1 text-base sm:text-xl	">
             {currencyFormatter(maxVariantPrice)}
           </h3>
-          <h3 className="font-bold	">{currencyFormatter(minVariantPrice)}</h3>
+          <h3 className="font-bold text-base sm:text-xl">
+            {currencyFormatter(minVariantPrice)}
+          </h3>
         </div>
       </div>
     </div>
