9e3d8678d9e088c42b338427b25977d3d6eba2ea
user <samuel_morgan_@outlook.com>
2023-09-29 12:41:35 -0300

    heading resp

--- pages/campaign/[handle]/collections/[collection_id]/index.tsx
+++ pages/campaign/[handle]/collections/[collection_id]/index.tsx
@@ -28,7 +28,7 @@ const CollectionCampaignPage = () => {
   return (
     <Page>
       <NavBar {...{ campaign, campaignHandle: handle, collections }} />
-      <h1 className="text-4xl text-black mx-1 my-8 mx-16 mr-auto ">
+      <h1 className="text-2xl sm:text-4xl text-black mx-1 my-4 sm:my-8 mx-16 mr-auto ">
         {selectedCollection?.title}
       </h1>
       <ProductGrid {...{ products: collectionProducts, handle }} />
