2b5ae2038940c193520d678df29354199a63731a
user <samuel_morgan_@outlook.com>
2023-09-29 15:48:29 -0300

    grid margin

--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -3,7 +3,7 @@ import Section from "./Section";
 const ProductGrid = ({ products, handle }) => {
   return (
     <Section>
-      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 items-center justify-center gap-8 w-full mx-16">
+      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 items-center justify-center gap-8 w-full mx-8 xs:mx-16">
         {products.map((product) => {
           return <ProductCard {...{ product, handle }} key={product.id} />;
         })}
