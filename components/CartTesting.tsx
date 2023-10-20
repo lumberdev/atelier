ed68e40e6c376a26086c7c8cadee5c5c4ceaf539
user <samuel_morgan_@outlook.com>
2023-10-20 18:22:24 -0300

    fixed cart testing errors

--- components/CartTesting.tsx
+++ components/CartTesting.tsx
@@ -15,47 +15,51 @@ const CartTesting = ({ campaign }) => {
       <h3 className="absolute p-2 top-0 right-0">CART TESTING</h3>
       <h2 className="text-xl mb-4">Cart items:</h2>
       <table className="w-full">
-        <tr>
-          <th>Item</th>
-          <th>ID</th>
-          <th>Quantity</th>
-          <th>Cart Controls</th>
-        </tr>
-        {cartItems.length > 0 ? (
-          cartItems.map((item, index) => (
-            <tr className="text-center">
-              <td>{`${item.title}`}</td>
-              <td>{`${item.id}`}</td>
-              <td className="text-center">{`${item.quantity}`}</td>
-              <td>
-                <button
-                  className="bg-[lightgreen] w-10 h-10 text-3xl uppercase text-black p-2 cursor-pointer inline-flex items-center justify-center"
-                  onClick={() => increaseItem(item)}
-                >
-                  +
-                </button>
-                <button
-                  className="bg-[pink] w-10 h-10 text-3xl uppercase text-black p-2 cursor-pointer inline-flex items-center justify-center"
-                  onClick={() => decreaseItem(item)}
-                >
-                  -
-                </button>
-                <button
-                  className="bg-[red] w-10 h-10 text-3xl uppercase text-white p-2 cursor-pointer inline-flex items-center justify-center"
-                  onClick={() => clearItem(item)}
-                >
-                  ✕
-                </button>
+        <thead>
+          <tr>
+            <th>Item</th>
+            <th>ID</th>
+            <th>Quantity</th>
+            <th>Cart Controls</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cartItems.length > 0 ? (
+            cartItems.map((item, index) => (
+              <tr className="text-center" key={index}>
+                <td>{`${item.title}`}</td>
+                <td className="break-all">{`${item.id}`}</td>
+                <td className="text-center">{`${item.quantity}`}</td>
+                <td>
+                  <button
+                    className="bg-[lightgreen] w-10 h-10 text-3xl uppercase text-black p-2 cursor-pointer inline-flex items-center justify-center"
+                    onClick={() => increaseItem(item)}
+                  >
+                    +
+                  </button>
+                  <button
+                    className="bg-[pink] w-10 h-10 text-3xl uppercase text-black p-2 cursor-pointer inline-flex items-center justify-center"
+                    onClick={() => decreaseItem(item)}
+                  >
+                    -
+                  </button>
+                  <button
+                    className="bg-[red] w-10 h-10 text-3xl uppercase text-white p-2 cursor-pointer inline-flex items-center justify-center"
+                    onClick={() => clearItem(item)}
+                  >
+                    ✕
+                  </button>
+                </td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td className="text-center pt-4" colSpan={3}>
+                No items in cart
               </td>
             </tr>
-          ))
-        ) : (
-          <tr>
-            <td className="text-center pt-4" colSpan={3}>
-              No items in cart
-            </td>
-          </tr>
-        )}
+          )}
+        </tbody>
       </table>
       <button
         className="bg-[darkred] w-full uppercase text-white py-2 px-4 rounded mt-4 cursor-pointer"
