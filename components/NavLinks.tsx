253abd9b6a7557825c1a3cef3333a174a98575d9
user <samuel_morgan_@outlook.com>
2023-10-23 19:33:41 -0400

    nav spacing;

--- components/NavLinks.tsx
+++ components/NavLinks.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 const NavLinks = ({ campaignHandle, collections, color }) => {
-  const linkStyle = `mx-4 sm:mx-2 first:ml-0 mb-2 sm:mb-0 no-underline hover:underline text-base whitespace-nowrap	 text-${color}`;
+  const linkStyle = `mx-4 lg:mx-2 first:lg:ml-0 first:ml-4 mb-2 lg:mb-0 no-underline hover:underline text-base whitespace-nowrap	 text-${color}`;
   return (
     <>
       <Link
