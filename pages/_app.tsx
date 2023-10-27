f6a6910e0d08a5a5ff491147515ef197240654bc
Luis Fernando Saavedra Meza <fernando@fsvdr.me>
2023-10-27 16:50:31 -0600

    feat: Pull config for password page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,10 @@
 import { NavigationMenu } from "@shopify/app-bridge-react";
 import { Frame, AppProvider as PolarisProvider } from "@shopify/polaris";
-import "@shopify/polaris/build/esm/styles.css";
 import translations from "@shopify/polaris/locales/en.json";
 import AppBridgeProvider from "../components/providers/AppBridgeProvider";
 import { useRouter } from "next/router";
 import { QueryClientProvider } from "react-query";
 import { queryClient } from "@/utils/queryClient";
-import "./global.css";
 import { CartProvider } from "@/context/CartContext";
 import SlidingCart from "@/components/cart/SlidingCart";
 
@@ -17,7 +15,10 @@ export default function App({ Component, pageProps }) {
   if (
     router.pathname.indexOf("/app") === 0 ||
     router.pathname.indexOf("/exitframe") === 0
-  )
+  ) {
+    import("./global.css" as any);
+    import("@shopify/polaris/build/esm/styles.css" as any);
+
     return (
       <PolarisProvider i18n={translations}>
         <AppBridgeProvider>
@@ -39,8 +40,10 @@ export default function App({ Component, pageProps }) {
         </AppBridgeProvider>
       </PolarisProvider>
     );
+  }
 
   // ATELIER WEBSITE
+  import("./site-global.css" as any);
   return (
     <QueryClientProvider client={queryClient}>
       <CartProvider>
