ec2b773116e70480e5838cc997b96d36f56701e8
morgan-sam <57941781+morgan-sam@users.noreply.github.com>
2023-11-10 16:44:05 -0500

    Bugfix/Campaign Form Non-RHF Controlled (#15)

--- components/CampaignForm.tsx
+++ components/CampaignForm.tsx
@@ -32,6 +32,7 @@ import {
 import { useRouter } from "next/router";
 import { FC, useCallback, useEffect, useState } from "react";
 import { Controller } from "react-hook-form";
+import { areArraysTheSame } from "@/lib/helper/objects";
 
 const CampaignForm: FC<{
   campaign?: campaigns;
@@ -39,8 +40,28 @@ const CampaignForm: FC<{
   products?: ProductResourceItem[];
 }> = ({ campaign, collections, products }) => {
   const router = useRouter();
+  const [nonControlledFormDirty, setNonControlledFormDirty] =
+    useState<boolean>(false);
+  const [imageChanged, setImageChanged] = useState<boolean>(false);
+  const {
+    isLoading,
+    imageUrl,
+    imageFile,
+    formState,
+    onSubmit,
+    setValue,
+    setImageFile,
+    watch,
+    reset,
+    control,
+  } = useCampaignForm(
+    campaign,
+    (isDirty: boolean) => setNonControlledFormDirty(isDirty),
+    (imageChanged: boolean) => setImageChanged(imageChanged)
+  );
+
   const [cartItemImageStyle, setCartItemImageStyle] = useState<string[]>([
-    "round",
+    formState.defaultValues.cartItemsImageStyle,
   ]);
 
   const handleCartItemImageStyleChange = useCallback(
@@ -68,19 +89,6 @@ const CampaignForm: FC<{
   const [selectedItems, setSelectedItems] = useState<
     ResourceListProps["selectedItems"]
   >([]);
-
-  const {
-    isLoading,
-    imageUrl,
-    imageFile,
-    formState,
-    onSubmit,
-    setValue,
-    setImageFile,
-    watch,
-    reset,
-    control,
-  } = useCampaignForm(campaign);
 
   const isActive = watch("isActive");
   const isActiveSelectValue = isActive ? "true" : "false";
@@ -115,10 +123,70 @@ const CampaignForm: FC<{
     setValue("variantIds", variants);
   }, [selectedTab, selectedProducts]);
 
+  const hasNonControllerInputsChanged = () => {
+    const formProductIds = selectedProducts.map((resource) => resource.id);
+    const defaultProductIds = [...campaign.productIds];
+    const formCollectionIds = selectedCollections.map(
+      (resource) => resource.id
+    );
+    const defaultCollectionIds = [...campaign.collectionIds];
+    if (cartItemImageStyle[0] != campaign.cartItemsImageStyle) {
+      return true;
+    } else if (campaign.isActive != isActive) {
+      return true;
+    } else if (!areArraysTheSame(formProductIds, defaultProductIds)) {
+      return true;
+    } else if (!areArraysTheSame(formCollectionIds, defaultCollectionIds)) {
+      return true;
+    } else if (imageChanged) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  useEffect(() => {
+    if (campaign) {
+      const nonControlledInputsChanged = hasNonControllerInputsChanged();
+      setNonControlledFormDirty(nonControlledInputsChanged);
+    }
+  }, [
+    cartItemImageStyle,
+    isActive,
+    selectedProducts,
+    selectedCollections,
+    imageChanged,
+    campaign,
+  ]);
+
+  const resetForm = () => {
+    if (campaign)
+      reset({
+        id: campaign.id,
+        title: campaign.title,
+        handle: campaign.handle,
+        description: campaign.description,
+        cartTitle: campaign.cartTitle,
+        cartBackgroundColor: campaign.cartBackgroundColor,
+        cartTextColor: campaign.cartTextColor,
+        cartItemsImageStyle: campaign.cartItemsImageStyle,
+        cartDescription: campaign.cartDescription,
+        isActive: campaign.isActive,
+        collectionIds: campaign.collectionIds,
+        productIds: campaign.productIds,
+        variantIds: campaign.variantIds,
+        password: campaign.password,
+      });
+  };
+
+  useEffect(() => {
+    if (campaign) resetForm();
+  }, [campaign]);
+
   return (
     <Frame>
       <ContextualSaveBar
-        visible={formState.isDirty}
+        visible={formState.isDirty || nonControlledFormDirty}
         saveAction={{
           disabled: !formState.isValid,
           onAction: onSubmit,
@@ -126,11 +194,12 @@ const CampaignForm: FC<{
         }}
         discardAction={{
           onAction: () => {
-            reset();
+            resetForm();
             router.back();
           },
         }}
       />
+
       <Form onSubmit={onSubmit}>
         <HorizontalGrid columns={{ xs: 1, md: "2fr 1fr" }} gap="4">
           <VerticalStack gap="4">
@@ -144,6 +213,7 @@ const CampaignForm: FC<{
                       label="Title"
                       autoComplete="off"
                       autoFocus
+                      disabled={isLoading}
                       {...field}
                     />
                   )}
@@ -153,7 +223,12 @@ const CampaignForm: FC<{
                   control={control}
                   name="handle"
                   render={({ field }) => (
-                    <TextField label="Handle" autoComplete="off" {...field} />
+                    <TextField
+                      label="Handle"
+                      autoComplete="off"
+                      disabled={isLoading}
+                      {...field}
+                    />
                   )}
                 />
 
@@ -165,6 +240,7 @@ const CampaignForm: FC<{
                       multiline={4}
                       label="Description"
                       autoComplete="off"
+                      disabled={isLoading}
                       {...field}
                     />
                   )}
@@ -188,6 +264,7 @@ const CampaignForm: FC<{
                         label="Cart Title"
                         autoComplete="off"
                         autoFocus
+                        disabled={isLoading}
                         {...field}
                       />
                     )}
@@ -200,6 +277,7 @@ const CampaignForm: FC<{
                         label="Background Color"
                         autoComplete="off"
                         autoFocus
+                        disabled={isLoading}
                         {...field}
                       />
                     )}
@@ -212,6 +290,7 @@ const CampaignForm: FC<{
                         label="Text Color"
                         autoComplete="off"
                         autoFocus
+                        disabled={isLoading}
                         {...field}
                       />
                     )}
@@ -224,6 +303,7 @@ const CampaignForm: FC<{
                     ]}
                     selected={cartItemImageStyle}
                     onChange={handleCartItemImageStyleChange}
+                    disabled={isLoading}
                   />
 
                   <Controller
@@ -234,6 +314,7 @@ const CampaignForm: FC<{
                         multiline={4}
                         label="Cart Description"
                         autoComplete="off"
+                        disabled={isLoading}
                         {...field}
                       />
                     )}
@@ -530,6 +611,7 @@ const CampaignForm: FC<{
                   ]}
                   value={isActiveSelectValue}
                   onChange={(value) => setValue("isActive", value === "true")}
+                  disabled={isLoading}
                 />
               </VerticalStack>
             </Card>
@@ -545,7 +627,10 @@ const CampaignForm: FC<{
                     <Button
                       destructive
                       plain
-                      onClick={() => setImageFile(null)}
+                      onClick={() => {
+                        setImageChanged(true);
+                        setImageFile(null);
+                      }}
                     >
                       Remove
                     </Button>
@@ -556,11 +641,15 @@ const CampaignForm: FC<{
                   accept="image/*"
                   type="image"
                   allowMultiple={false}
+                  disabled={isLoading}
                   onDrop={(
                     _dropFiles: File[],
                     acceptedFiles: File[],
                     _rejectedFiles: File[]
-                  ) => setImageFile(acceptedFiles[0])}
+                  ) => {
+                    setImageChanged(true);
+                    setImageFile(acceptedFiles[0]);
+                  }}
                 >
                   {imageUrl && (
                     <HorizontalStack>
@@ -595,6 +684,7 @@ const CampaignForm: FC<{
                     <TextField
                       label="Password"
                       autoComplete="false"
+                      disabled={isLoading}
                       {...field}
                     />
                   )}
--- lib/hooks/useCampaignForm.ts
+++ lib/hooks/useCampaignForm.ts
@@ -2,13 +2,14 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { useEffect, useState } from "react";
-import { useMutation } from "react-query";
+import { QueryClient, useMutation } from "react-query";
 import useFetch from "@/components/hooks/useFetch";
 import { supabaseStorage } from "@/utils/supabase";
 import { useStoreSettings } from "./useStoreSettings";
 import { CampaignInput } from "../types";
 import { useRouter } from "next/router";
 import { campaigns } from "@prisma/client";
+import { queryClient } from "@/utils/queryClient";
 
 const schema = yup
   .object({
@@ -31,7 +32,11 @@ const schema = yup
   })
   .required();
 
-export const useCampaignForm = (campaign?: campaigns) => {
+export const useCampaignForm = (
+  campaign?: campaigns,
+  setNonControlledFormDirty?: (isDirty: Boolean) => void,
+  setImageChanged?: (imageChanged: Boolean) => void
+) => {
   const router = useRouter();
   const fetch = useFetch();
   const {
@@ -76,9 +81,27 @@ export const useCampaignForm = (campaign?: campaigns) => {
 
         const campaign = response.campaign;
 
-        form.reset();
+        form.reset({
+          id: campaign.id,
+          title: campaign.title,
+          handle: campaign.handle,
+          description: campaign.description,
+          cartTitle: campaign.cartTitle,
+          cartBackgroundColor: campaign.cartBackgroundColor,
+          cartTextColor: campaign.cartTextColor,
+          cartItemsImageStyle: campaign.cartItemsImageStyle,
+          cartDescription: campaign.cartDescription,
+          isActive: campaign.isActive,
+          collectionIds: campaign.collectionIds,
+          productIds: campaign.productIds,
+          variantIds: campaign.variantIds,
+          password: campaign.password,
+        });
         setIsLoading(false);
         router.push(`/app/campaign/${campaign.id}`);
+        queryClient.refetchQueries(["campaign", campaign.id]);
+        setNonControlledFormDirty(false);
+        setImageChanged(false);
       },
     }
   );
@@ -87,13 +110,17 @@ export const useCampaignForm = (campaign?: campaigns) => {
     setIsLoading(true);
 
     if (imageFile) {
-      // 1. Upload image
       const [shopId] = shop.split(".");
-      const fileName = fields.handle;
-      const [fileExt] = imageFile.name.split(".").reverse();
 
+      // delete campaign.image from supa storage
+      if (campaign?.image) {
+        const image = supabaseStorage.getPublicUrl(campaign.image);
+        const imageKey = image.data.publicUrl.split("/").reverse()[0];
+        await supabaseStorage.remove([`${shopId}/${imageKey}`]);
+      }
+      // upload image to supa storage
       const storageResponse = await supabaseStorage.upload(
-        `${shopId}/${fileName}.${fileExt}`,
+        `${shopId}/${imageFile.name.replaceAll(" ", "_")}`,
         imageFile,
         { upsert: true }
       );
@@ -101,11 +128,9 @@ export const useCampaignForm = (campaign?: campaigns) => {
 
       // 2. Upload data
       upsertCampaign({ data: { ...fields, image } });
-
-      return;
+    } else {
+      upsertCampaign({ data: { ...fields } });
     }
-
-    upsertCampaign({ data: { ...fields } });
   });
 
   useEffect(() => {
