ee6eb3a13f2abf5da7e38e11dd816577595ef74e
Luis Fernando Saavedra Meza <fernando@fsvdr.me>
2023-11-09 18:32:10 -0600

    fix: Fix store theme update

--- pages/api/apps/store-themes/index.ts
+++ pages/api/apps/store-themes/index.ts
@@ -21,14 +21,12 @@ router.get(async (req, res) => {
   });
 
   if (!store)
-    return res
-      .status(500)
-      .json({
-        error: {
-          code: "UNABLE_TO_FIND_STORE",
-          message: "Unable to fund a store with the provided name",
-        },
-      });
+    return res.status(500).json({
+      error: {
+        code: "UNABLE_TO_FIND_STORE",
+        message: "Unable to fund a store with the provided name",
+      },
+    });
 
   return res.status(200).json({ theme: store?.theme });
 });
@@ -37,22 +35,18 @@ router.post(async (req, res) => {
   const shop = req.user_session.shop;
   const body = JSON.parse(req.body) as StoreThemeInput;
 
-  const theme = await prisma.storeThemes.upsert({
-    where: { id: body.id ?? "" },
-    create: {
-      store: {
-        connect: {
-          shop,
-        },
-      },
-      ...body,
-    },
-    update: {
-      ...body,
-    },
+  const store = await prisma.stores.findUnique({
+    where: { shop },
+    include: { theme: true },
+  });
+  const theme = store.theme;
+
+  const updatedTheme = await prisma.storeThemes.update({
+    where: { id: theme.id },
+    data: { ...body },
   });
 
-  return res.status(200).json({ theme });
+  return res.status(200).json({ theme: updatedTheme });
 });
 
 export default router.handler();
