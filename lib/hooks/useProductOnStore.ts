b9af204a9459a9ae21efc3e6c6ca191edba1d8da
user <samuel_morgan_@outlook.com>
2023-11-13 23:17:37 -0500

    single prod hooks params;

--- lib/hooks/useProductOnStore.ts
+++ lib/hooks/useProductOnStore.ts
@@ -5,23 +5,23 @@ export const useProductOnStore = ({ store_id, product_id }) => {
     product: any;
   }>(
     ["product", store_id, product_id],
-    () =>
-      fetch(`/api/product?store_id=${store_id}&product_id=${product_id}/`)
-        .then((response) => response.json())
-        .then((data) => {
-          return {
-            product: {
-              id: data.product.id,
-              title: data.product.title,
-              description: data.product.description,
-              priceRangeV2: data.product.priceRangeV2,
-              options: data.product.options,
-              handle: data.product.handle,
-              images: data.product.images.edges.map((edge) => edge.node),
-              variants: data.product.variants.edges.map((edge) => edge.node),
-            },
-          };
-        }),
+    async () => {
+      const params = new URLSearchParams({ store_id, product_id });
+      const response = await fetch(`/api/product?${params.toString()}`);
+      const data = (await response.json()) as any;
+      return {
+        product: {
+          id: data.product.id,
+          title: data.product.title,
+          description: data.product.description,
+          priceRangeV2: data.product.priceRangeV2,
+          options: data.product.options,
+          handle: data.product.handle,
+          images: data.product.images.edges.map((edge) => edge.node),
+          variants: data.product.variants.edges.map((edge) => edge.node),
+        },
+      };
+    },
     {
       enabled: !!store_id && !!product_id,
     }
