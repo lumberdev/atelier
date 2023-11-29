06aab6d5c46d4552f6ea515b4d96219a0744b765
user <samuel_morgan_@outlook.com>
2023-11-29 18:04:17 -0500

    auto redirects

--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -73,7 +73,7 @@ const AppHomePage = () => {
                 primaryAction={{
                   content: "Configure domain",
                   onAction: () => {
-                    router.push("/app/settings");
+                    router.push("/app/settings?initial=true");
                   },
                 }}
                 illustration="https://cdn.shopify.com/s/assets/admin/checkout/settings-customizecart-705f57c725ac05be5a34ec20c05b94298cb8afd10aac7bd9c7ad02030f48cfa0.svg"
