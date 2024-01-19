14f2a3a68fe82499fbfefdf8a50d2b4134307bac
Luis Fernando Saavedra Meza <fernando@fsvdr.me>
2024-01-19 14:19:16 -0600

    Display product listing details

---
+++ lib/hooks/app/useShop.ts
@@ -0,0 +1,35 @@
+import useFetch from "@/components/hooks/useFetch";
+import { useQuery } from "react-query";
+
+const useShop = () => {
+  const fetch = useFetch();
+
+  const {
+    data = {
+      id: "",
+      domain: "",
+      appHandle: "",
+      publicationId: "",
+      availableProductCount: 0,
+    },
+    isLoading,
+  } = useQuery<
+    {},
+    any,
+    {
+      id: string;
+      domain: string;
+      appHandle: string;
+      publicationId: string;
+      availableProductCount: number;
+    }
+  >({
+    queryKey: "shop",
+    queryFn: () => fetch("/api/apps/shop").then((response) => response.json()),
+    onSuccess: (response) => {},
+  });
+
+  return { ...data, isLoading };
+};
+
+export default useShop;
