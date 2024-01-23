50b51bb89283a877d8a13cb1b8c73bc5b10f6fc1
Luis Fernando Saavedra Meza <fernando@fsvdr.me>
2024-01-22 18:52:42 -0600

    feat: Implement new campaign page

---
+++ components/campaign/Page.tsx
@@ -0,0 +1,189 @@
+import { useStoreSettings } from "@/lib/hooks/app/useStoreSettings";
+import { CampaignCollection } from "@/lib/types";
+import {
+  CallbackAction,
+  Card,
+  EmptyState,
+  Form,
+  Frame,
+  HorizontalGrid,
+  Layout,
+  LinkAction,
+  Page,
+  Select,
+  SkeletonThumbnail,
+  Text,
+  VerticalStack,
+} from "@shopify/polaris";
+import { ExternalMinor } from "@shopify/polaris-icons";
+import { FC } from "react";
+import StoreCustomizationFormSlice from "./StoreCustomizationFormSlice";
+import CartCustomizationFormSlice from "./CartCustomizationFormSlice";
+import ProductListing from "./ProductListing";
+import { useCampaignForm } from "@/lib/hooks/app/useCampaignForm";
+import { ContextualSaveBar } from "@shopify/app-bridge-react";
+import { useRouter } from "next/router";
+import { Controller } from "react-hook-form";
+
+const CampaignPage: FC<{
+  collection: CampaignCollection;
+  backAction?: CallbackAction | LinkAction;
+}> = ({ collection, backAction }) => {
+  const router = useRouter();
+  const {
+    settings: { shop },
+  } = useStoreSettings();
+  const { control, isLoading, formState, reset, onSubmit } = useCampaignForm();
+
+  const collectionUrl = `https://${shop}/admin/collections/${collection.id
+    .split("/")
+    .reverse()
+    .at(0)}`;
+
+  return (
+    <Frame>
+      <ContextualSaveBar
+        visible={formState.isDirty}
+        saveAction={{
+          disabled: !formState.isValid,
+          onAction: onSubmit,
+          loading: isLoading,
+        }}
+        discardAction={{
+          onAction: () => {
+            reset();
+            router.back();
+          },
+        }}
+      />
+
+      <Page
+        title={collection.title}
+        backAction={backAction}
+        secondaryActions={[
+          {
+            content: "Manage collection",
+            external: true,
+            url: collectionUrl,
+          },
+        ]}
+      >
+        <Form onSubmit={onSubmit}>
+          <Layout>
+            <Layout.Section>
+              <HorizontalGrid columns={{ xs: 1, md: "2fr 1fr" }} gap="4">
+                {/* LEFT PANEL */}
+                <VerticalStack gap="4">
+                  {collection.descriptionHtml ? (
+                    <div
+                      dangerouslySetInnerHTML={{
+                        __html: collection.descriptionHtml,
+                      }}
+                    />
+                  ) : (
+                    <EmptyState
+                      image=""
+                      action={{
+                        content: "Go to Collection",
+                        icon: ExternalMinor,
+                        external: true,
+                        url: collectionUrl,
+                      }}
+                    >
+                      <Text as="p">
+                        Your campaign will use the {collection.title}&nbsp;
+                        collection's details such as description and image. You
+                        can configure the rest of the campaign now and update
+                        the collection at any moment.
+                      </Text>
+                    </EmptyState>
+                  )}
+                  <Text as="p">{collection.description}</Text>
+
+                  <ProductListing />
+                </VerticalStack>
+
+                {/* RIGHT PANEL */}
+                <VerticalStack gap="4">
+                  <Card>
+                    <VerticalStack gap="4">
+                      <Text as="h2" variant="headingMd">
+                        Image
+                      </Text>
+
+                      {collection.image ? (
+                        <SkeletonThumbnail />
+                      ) : (
+                        <Text as="p" variant="bodySm">
+                          Configure the campaign image on the collection page
+                        </Text>
+                      )}
+                    </VerticalStack>
+                  </Card>
+
+                  <Card>
+                    <VerticalStack gap="4">
+                      <Text as="h2" variant="headingMd">
+                        Status
+                      </Text>
+
+                      <Controller
+                        control={control}
+                        name="isActive"
+                        render={({ field: { value, onChange, ...field } }) => (
+                          <Select
+                            label=""
+                            options={[
+                              { label: "Active", value: "true" },
+                              { label: "Draft", value: "false" },
+                            ]}
+                            value={String(value)}
+                            onChange={(value) => onChange(value === "true")}
+                            disabled={isLoading}
+                            {...field}
+                          />
+                        )}
+                      />
+                    </VerticalStack>
+                  </Card>
+                </VerticalStack>
+              </HorizontalGrid>
+
+              <div className="h-8" />
+            </Layout.Section>
+
+            <Layout.AnnotatedSection
+              title="Access Control"
+              description="Restrict access to your campaign page. Customers will have to enter the password to access your product listing."
+            >
+              {/* <CampaignAccessControlFormSlice campaign={campaign} /> */}
+            </Layout.AnnotatedSection>
+
+            <Layout.AnnotatedSection
+              title="Store Customization"
+              description="Customize your campaign page"
+            >
+              <StoreCustomizationFormSlice
+                control={control}
+                isLoading={isLoading}
+              />
+            </Layout.AnnotatedSection>
+
+            <Layout.AnnotatedSection
+              title="Cart Customization"
+              description="Customize the cart"
+            >
+              <CartCustomizationFormSlice
+                control={control}
+                isLoading={isLoading}
+              />
+              <div className="h-16" />
+            </Layout.AnnotatedSection>
+          </Layout>
+        </Form>
+      </Page>
+    </Frame>
+  );
+};
+
+export default CampaignPage;
--- pages/app/campaign/new.tsx
+++ pages/app/campaign/new.tsx
@@ -1,23 +1,26 @@
-import CampaignForm from "@/components/CampaignForm";
-import { Page } from "@shopify/polaris";
+import CampaignPage from "@/components/campaign/Page";
+import CampaignSkeletonPage from "@/components/campaign/SkeletonPage";
+import useCollection from "@/lib/hooks/app/useCollection";
 import { useRouter } from "next/router";
 
 const NewCampaignPage = () => {
   const router = useRouter();
+  const { isLoading, ...collection } = useCollection(
+    router.query.collection_id as string
+  );
+
+  if (isLoading) return <CampaignSkeletonPage />;
 
   return (
-    <Page
-      title="Add Campaign"
+    <CampaignPage
+      collection={collection}
       backAction={{
         content: "Campaigns",
         onAction: () => {
           router.back();
         },
       }}
-    >
-      <CampaignForm />
-      <div className="h-16" />
-    </Page>
+    />
   );
 };
 
