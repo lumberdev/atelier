7263262f8b14f8df7baf8047b0eb68cb086fd459
Pratik <pratikmndh@gmail.com>
2024-02-13 15:55:10 -0500

    chore: bootstrap tina cms

--- middleware.js
+++ middleware.js
@@ -25,6 +25,7 @@ export function middleware(request) {
   const shop = params.shop || "*.myshopify.com";
 
   const res = NextResponse.next();
+  console.log(pathname)
 
   // Exclude "/admin/index.html" and "/", this causes tinacms admin panel and live preview to crash 
   if (!(pathname.includes("/admin/index.html") || pathname === "/")) {
