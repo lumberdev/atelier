f186ebc2fe6b9e3285cc713a0d05408e6cc2d3f3
Luis Fernando Saavedra Meza <fernando@fsvdr.me>
2024-02-13 20:42:16 -0600

    feat: Add icons

--- next.config.js
+++ next.config.js
@@ -16,15 +16,15 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'cdn.shopify.com',
-        port: '',
-        pathname: '/*/**',
+        protocol: "https",
+        hostname: "cdn.shopify.com",
+        port: "",
+        pathname: "/*/**",
       },
       {
-        protocol: 'https',
-        hostname: '**.supabase.co',
-      }
+        protocol: "https",
+        hostname: "**.supabase.co",
+      },
     ],
   },
   experimental: {
@@ -36,6 +36,37 @@ const nextConfig = {
         },
       ],
     ],
+  },
+  webpack(config, { isServer }) {
+    // Configures webpack to handle SVG files with SVGR. SVGR optimizes and transforms SVG files
+    // into React components. See https://react-svgr.com/docs/next/
+
+    // Grab the existing rule that handles SVG imports
+    // @ts-ignore - rules is a private property that is not typed
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.(".svg")
+    );
+
+    config.module.rules.push(
+      // Reapply the existing rule, but only for svg imports ending in ?url
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      // Convert all other *.svg imports to React components
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+        use: ["@svgr/webpack"],
+      }
+    );
+
+    // Modify the file loader rule to ignore *.svg, since we have it handled now.
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
   },
 };
 
