bd5cb9577fa3b465f84ceeba430cd3f3075c838d
Luis Fernando Saavedra Meza <fernando@fsvdr.me>
2024-02-19 19:23:43 -0600

    feat: Handle default values and image url transforms server side

--- components/LogoTitle.tsx
+++ components/LogoTitle.tsx
@@ -1,7 +1,5 @@
-import { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { supabaseStorage } from "@/utils/supabase";
 import { useTheme } from "@/context/ThemeProvider";
 
 const LogoTitle = ({ title, handle, color = "black", className = "" }) => {
@@ -9,24 +7,16 @@ const LogoTitle = ({ title, handle, color = "black", className = "" }) => {
     global: { logo },
   } = useTheme();
 
-  const [imageUrl, setImageUrl] = useState<string>("");
-
-  useEffect(() => {
-    if (!logo) return;
-    const image = supabaseStorage.getPublicUrl(logo);
-    setImageUrl(image?.data.publicUrl ?? "");
-  }, [logo]);
-
   return (
     <Link
       href={`/${handle}`}
       className={`flex items-center no-underline ${className}`}
     >
-      {imageUrl !== "" ? (
+      {logo ? (
         <Image
           width={100}
           height={100}
-          src={imageUrl}
+          src={logo}
           alt={title}
           className="mx-auto h-[3rem] w-auto py-2 lg:mx-0"
         />
