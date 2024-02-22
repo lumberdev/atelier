d7e9c0cdc422865709780e961a8afe482e588472
Luis Fernando Saavedra Meza <fernando@fsvdr.me>
2024-02-22 16:32:06 -0600

    chore: Remove unused code

--- components/NotFoundPage.tsx
+++
@@ -1,9 +0,0 @@
-const NotFoundPage = () => {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
-      <h1 className="mb-2 text-2xl font-medium">This page is not active.</h1>
-    </div>
-  );
-};
-
-export default NotFoundPage;
