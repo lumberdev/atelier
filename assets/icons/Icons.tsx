76b4da7b6a0d0eb8b15d0cdc9dfae84a8f8a8d79
slee288 <kingdog6694@gmail.com>
2024-03-25 12:36:04 -0400

    Revert "App uninstallation webhook data deletion"
    This reverts commit c42a56b9cbc2201ac5e7e54fa1c375f2b4104831.

--- assets/icons/Icons.tsx
+++ assets/icons/Icons.tsx
@@ -7,7 +7,7 @@ const NavigationMenu = ({
 }) => {
     return (
         <svg width={width} height={height} viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M1 10H15M1 4H19M1 16H19" stroke={stroke} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M1 10H15M1 4H19M1 16H19" stroke={stroke} stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
         </svg>
     )
 }
